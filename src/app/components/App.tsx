8fc238b5f525ceda5d76336bb0ff7c534b4e1162
Alex <alex@vizku.nz>
2024-08-12 23:36:51 +1200

    added button events

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -11,31 +11,37 @@ const palettes = [
       "seed": ['123456'],
       "comment": 'Thise is a test comment',
       "controls": [{
+        "type": 'edit', 
         "name": 'edit', 
         "position": '1', 
         "tooltip": 'Edit Palette'
       }, 
       {
+        "type": 'link', 
         "name": 'link', 
         "position": '2', 
         "tooltip": 'Create Linked Palettes'
       }, 
       {
+        "type": 'swap', 
         "name": 'swap_horiz', 
         "position": '2', 
         "tooltip": 'Reveres Palette'
       }, 
       {
+        "type": 'up', 
         "name": 'arrow_upward', 
         "position": '2', 
         "tooltip": 'Move Palette one up'
       }, 
       {
+        "type": 'down', 
         "name": 'arrow_downward', 
         "position": '2', 
         "tooltip": 'Move Palette one down'
       }, 
       {
+        "type": 'delete', 
         "name": 'delete', 
         "position": '2', 
         "tooltip": 'Delete Palette'
@@ -50,21 +56,25 @@ const palettes = [
       "seed": ['123456'],
       "comment": 'Thise is a test comment',
       "controls": [{
+        "type": 'swap', 
         "name": 'swap_horiz', 
         "position": '2', 
         "tooltip": 'Reveres Palette'
       }, 
       {
+        "type": 'up', 
         "name": 'arrow_upward', 
         "position": '2', 
         "tooltip": 'Move Palette one up'
       }, 
       {
+        "type": 'down', 
         "name": 'arrow_downward', 
         "position": '2', 
         "tooltip": 'Move Palette one down'
       }, 
       {
+        "type": 'delete', 
         "name": 'delete', 
         "position": '2', 
         "tooltip": 'Delete Palette'
@@ -72,8 +82,42 @@ const palettes = [
   },  
   "colours": [{"value": '#763916', "id": '123456'}, {"value": '#96fe11', "id": '456789'}]
   },
+  {
+  "meta": {
+      "title": 'palette 3',
+      "type": 'sequential',
+      "seed": ['384321'],
+      "comment": 'Thise is a test comment',
+      "controls": [{
+        "type": 'swap', 
+        "name": 'swap_horiz', 
+        "position": '2', 
+        "tooltip": 'Reveres Palette'
+      }, 
+      {
+        "type": 'up', 
+        "name": 'arrow_upward', 
+        "position": '2', 
+        "tooltip": 'Move Palette one up'
+      }, 
+      {
+        "type": 'down', 
+        "name": 'arrow_downward', 
+        "position": '2', 
+        "tooltip": 'Move Palette one down'
+      }, 
+      {
+        "type": 'delete', 
+        "name": 'delete', 
+        "position": '2', 
+        "tooltip": 'Delete Palette'
+      }]
+  },  
+  "colours": [{"value": '#369852', "id": '46548'}, {"value": '#153874', "id": '318494'}]
+  }
 ]
 
+
 export default function App() {
-  return <Palettes palettes={palettes} />;
+  return <Palettes paletteContent={palettes} />;
 }
--- src/app/components/paletteDisplay.tsx
+++ src/app/components/paletteDisplay.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useState } from 'react';
 
 function Colour({colour}){
   return(
@@ -8,8 +9,6 @@ function Colour({colour}){
     </div>
   )
 }
-
-
   
 function Colours({colours}){
   const rows = []
@@ -20,7 +19,6 @@ function Colours({colours}){
             key = {colour.value} />
     )
   })
-
   return(
       <div className="colours">
           {rows}
@@ -28,29 +26,76 @@ function Colours({colours}){
   )
 }
   
-function Control({control}){
+function Control({index, control, palette, setPalette, palettes, setPalettes}){
+  function handleOnClick() {
+    switch  (control.type) {
+      case 'edit':
+        console.log('edit')
+        break;
+      case 'link':
+        console.log('link')
+        break;
+      case 'swap':
+        setPalette({
+          ...palette,
+          colours: palette.colours.toReversed()
+        })
+        break;
+      case 'up':
+        if (index <= 0 || index > palettes.length - 1) {
+          console.log('Cannot be moved up')
+        } else {
+          let newPalettes = palettes.slice()
+          const element = newPalettes.splice(index, 1)[0];
+          newPalettes.splice(index - 1, 0, element);
+          setPalettes(newPalettes)
+        }
+        console.log('up')
+        break;
+      case 'down':
+        if (index < 0 || index >= palettes.length - 1) {
+          console.log('Cannot be moved down')
+        } else {
+          let newPalettes = palettes.slice()
+          const element = newPalettes.splice(index, 1)[0];
+          newPalettes.splice(index + 1, 0, element);
+          setPalettes(newPalettes)
+        }
+        break;
+      case 'delete':
+        let newPalettes = palettes.slice()
+        newPalettes.splice(index, 1)
+        setPalettes(newPalettes)
+        break;
+    }
+  }
+  
   return(
       <div className="control">
           <i 
             className="material-symbols-outlined"
-            title={control.tooltip} >
+            title={control.tooltip} 
+            onClick={handleOnClick} >
             {control.name}</i>
       </div>
   )
 }
   
-function Controls({controls}){
+function Controls({index, controls, palette, setPalette, palettes, setPalettes}){
   const rows = []
-
   controls.forEach((control) => {
       rows.push(
           <Control 
+              index = {index}
               control = {control}
-              key = {control.name} />
+              key = {control.name}
+              palette = {palette}
+              setPalette = {setPalette}
+              palettes = {palettes}
+              setPalettes = {setPalettes} />
       )
 
   })
-
   return(
       <div className="controls">
           {rows}
@@ -58,37 +103,64 @@ function Controls({controls}){
   )
 }
   
-function Meta({meta}){
+function Meta({index, meta, palette, setPalette, palettes, setPalettes}){
+  function handleOnChange() {
+    setPalette({
+      ...palette,
+      meta.title: palette.colours.toReversed()
+    })
+  }
   return(
       <div className="meta">
-          <input type="text" className="title" value={meta.title}></input>
+          <input 
+            type="text" 
+            className="title" 
+            value={meta.title}
+            onChange={handleOnChange}></input>
           <div className="type">{meta.type}</div>
-          <Controls controls = {meta.controls}/>
+          <Controls 
+            index = {index}
+            controls = {meta.controls}
+            palette = {palette}
+            setPalette = {setPalette}
+            palettes = {palettes}
+            setPalettes = {setPalettes}/>
       </div>
   )
 }
   
-function Palette({palette}){
+function Palette({paletteContent, index, palettes, setPalettes}){
+  const [palette, setPalette] = useState(paletteContent);
   return(
       <div className="palette">
-          <Meta meta = {palette.meta}/>
+          <Meta 
+            index = {index}
+            meta = {palette.meta}
+            palette = {palette}
+            setPalette = {setPalette}
+            palettes = {palettes}
+            setPalettes = {setPalettes}/>
           <Colours colours = {palette.colours}/>
       </div>
   )
 }
   
-export default function Palettes ( {palettes}) {
+export default function Palettes ( {paletteContent}) {
+  const [palettes, setPalettes] = useState(paletteContent);
     const rows = []
     
-    palettes.forEach((palette) => {
+    palettes.forEach((palette, index) => {
         rows.push(
             <Palette 
-                palette = {palette}
-                key = {palette.meta.title} />
+                paletteContent = {palette}
+                index = {index}
+                key = {palette.meta.title} 
+                palettes = {palettes}
+                setPalettes = {setPalettes}/>
         )
   
     })
-    console.log('palettes', palettes)
+    //console.log('palettes', palettes)
     return (
       <div>
         <div>Header!?</div>

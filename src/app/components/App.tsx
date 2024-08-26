56257c23b7ccaa19b0912923701c86e2b08d187c
Alex <alex@vizku.nz>
2024-08-27 09:14:48 +1200

    Generalise more - test deploy

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -36,7 +36,7 @@ export default function App() {
           alignItems="center"
           spacing={0.1}
           sx={{width: 798}}>
-            <Typography color="primary" level="h1" >Style Manager for Tableau - TEST1</Typography> 
+            <Typography color="primary" level="h1" >Style Manager for Tableau - TEST 2</Typography> 
             <HelpModal />
         </Stack>
         <Tabs 
